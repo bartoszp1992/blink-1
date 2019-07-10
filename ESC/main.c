8f46e993e05c28a2dd36f0074e5355dd1223310b
Bartosz <bartosz.pracz.92@gmail.com>
2019-07-10 13:35:58 +0200

    initial git release

---
+++ ESC/main.c
@@ -0,0 +1,454 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 lut 2018
+ *      Author: Bartosz Pracz
+ *     blink-1 ESC
+ *
+ *      v0.3 - correct throttle scale
+ *      v0.4 - only high side on PWM
+ *      v0.5 - beta faster sample rate
+ *      v0.7 - kers support
+ *      v0.8 - better measurement
+ *      v0.9 - experimental- very fast PWM
+ *      Milestone-1 awww, silence!
+ *      v0.95 - Hall error turns off all phases
+ *
+ */
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+//U//drivers
+# define U_H1 TCCR1A |= (1<<COM1A1); //pwm off
+# define U_H2 PORTD |= (1<<PD1); //high
+#define U_H3 PORTB |= (1 << PB1); //sd on
+
+#define U_H U_H2; U_H1; U_H3;
+
+# define U_L1 TCCR1A &= ~(1<<COM1A1); //pwm on
+# define U_L2 PORTD &= ~(1<<PD1); // low
+#define U_L3 PORTB |= (1 << PB1); //sd on
+
+#define U_L U_L2; U_L1; U_L3;
+
+# define U_OFF1 TCCR1A &= ~(1<<COM1A1); //pwm off
+# define U_OFF2 PORTD &= ~(1<<PD1); // low
+#define U_OFF3 PORTB &= ~(1 << PB1); //sd off
+
+#define U_OFF U_OFF2; U_OFF1; U_OFF3;
+
+//V
+# define V_H1 TCCR0A |= (1<<COM0A1); //pwm off
+# define V_H2 PORTD |= (1<<PD2); //high
+#define V_H3 PORTD |= (1<<PD6);//sd on
+
+#define V_H V_H2; V_H1; V_H3;
+
+# define V_L1 TCCR0A &= ~(1<<COM0A1);//pwm on
+# define V_L2 PORTD &= ~(1<<PD2);//low
+#define V_L3 PORTD |= (1<<PD6);//sd on
+
+#define V_L V_L2; V_L1; V_L3;
+
+# define V_OFF1 TCCR0A &= ~(1<<COM0A1);//pwm off
+# define V_OFF2 PORTD &= ~(1<<PD2);//low
+#define V_OFF3 PORTD &= ~(1<<PD6);//sd off
+
+#define V_OFF V_OFF2; V_OFF1; V_OFF3;
+
+//W
+# define W_H1 TCCR2A |= (1<<COM2A1); //pwm off
+# define W_H2 PORTD |= (1<<PD3); //high
+#define W_H3 PORTB |= (1<<PB3);//sd on
+
+#define W_H W_H2; W_H1; W_H3;
+
+# define W_L1 TCCR2A &= ~(1<<COM2A1); //pwm on
+# define W_L2 PORTD &= ~(1<<PD3); //low
+#define W_L3 PORTB |= (1<<PB3);//sd on
+
+#define W_L W_L2; W_L1; W_L3;
+
+# define W_OFF1 TCCR2A &= ~(1<<COM2A1);//pwm off
+# define W_OFF2 PORTD &= ~(1<<PD3); //low
+#define W_OFF3 PORTB &= ~(1<<PB3);//sd off
+
+#define W_OFF W_OFF2; W_OFF1; W_OFF3;
+
+#define ALL_OFF U_OFF; V_OFF; W_OFF;
+
+#define HALL1 ADMUX |= (1<<MUX0); ADMUX &= ~(1<<MUX1);
+#define HALL2 ADMUX &= ~(1<<MUX0); ADMUX |= (1<<MUX1);
+#define HALL3 ADMUX |= (1<<MUX0); ADMUX |= (1<<MUX1);
+
+#define one (PINB & (1<<PB7))
+#define two (PIND & (1<<PD5))
+#define three (PIND & (1<<PD7))
+#define four (PINB & (1<<PB0))
+
+#define oneOff !(PINB & (1<<PB7))
+#define twoOff !(PIND & (1<<PD5))
+#define threeOff !(PIND & (1<<PD7))
+#define fourOff !(PINB & (1<<PB0))
+
+int treshold = 400;
+
+volatile int enable;
+volatile int phase;
+volatile int toggle = 0;
+volatile int pulse;
+volatile int kers;
+int kersPower = 150;
+
+volatile int reading1;
+volatile int reading2;
+volatile int reading3;
+
+volatile int sensor1;
+volatile int sensor2;
+volatile int sensor3;
+
+int duty[] =
+		{ 0, 15, 30, 45, 60, 75, 90, 115, 130, 145, 160, 190, 220, 245, 255 };
+
+void measurement() {
+	HALL1
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	reading1 = ADC;
+
+	if (reading1 > treshold) {
+		sensor1 = 1;
+		//PORTC |= (1 << PC5);
+	} else {
+		sensor1 = 0;
+		//PORTC &= ~(1 << PC5);
+	}
+
+	HALL2
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	reading2 = ADC;
+
+	if (reading2 > treshold) {
+		sensor2 = 1;
+		PORTC |= (1 << PC5);
+	} else {
+		sensor2 = 0;
+		PORTC &= ~(1 << PC5);
+	}
+
+	HALL3
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	reading3 = ADC;
+
+	if (reading3 > treshold) {
+		sensor3 = 1;
+		//PORTC |= (1 << PC5);
+	} else {
+		sensor3 = 0;
+		//PORTC &= ~(1 << PC5);
+	}
+
+}
+
+void rotation(volatile int step) {
+
+	//if(step == 1) PORTC |= (1<<PC5);
+	//else PORTC &= ~(1<<PC5);
+
+	if (step == 0) {
+		W_OFF
+		U_L
+		V_H
+	}
+	if (step == 1) {
+		V_OFF
+		U_L
+		W_H
+	}
+	if (step == 2) {
+		U_OFF
+		V_L
+		W_H
+	}
+	if (step == 3) {
+		W_OFF
+		U_H
+		V_L
+	}
+	if (step == 4) {
+		V_OFF
+		U_H
+		W_L
+	}
+	if (step == 5) {
+		U_OFF
+		V_H
+		W_L
+	}
+
+}
+
+void power(void) {
+	if (one && two && three && four) {
+		pulse = duty[0];
+		enable = 0;
+		kers = 1;
+	}
+	if (one && two && three && fourOff) {
+		pulse = duty[0];
+		enable = 0;
+		kers = 0;
+	}
+	if (one && two && threeOff && four) {
+		pulse = duty[1];
+		enable = 1;
+		kers = 0;
+	}
+	if (one && two && threeOff && fourOff) {
+		pulse = duty[2];
+		enable = 1;
+		kers = 0;
+	}
+	if (one && twoOff && three && four) {
+		pulse = duty[3];
+		enable = 1;
+		kers = 0;
+	}
+	if (one && twoOff && three && fourOff) {
+		pulse = duty[4];
+		enable = 1;
+		kers = 0;
+	}
+	if (one && twoOff && threeOff && four) {
+		pulse = duty[5];
+		enable = 1;
+		kers = 0;
+	}
+	if (one && twoOff && threeOff && fourOff) {
+		pulse = duty[6];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && two && three && four) {
+		pulse = duty[7];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && two && three && fourOff) {
+		pulse = duty[8];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && two && threeOff && four) {
+		pulse = duty[9];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && two && threeOff && fourOff) {
+		pulse = duty[10];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && twoOff && three && four) {
+		pulse = duty[11];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && twoOff && three && fourOff) {
+		pulse = duty[12];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && twoOff && threeOff && four) {
+		pulse = duty[13];
+		enable = 1;
+		kers = 0;
+	}
+	if (oneOff && twoOff && threeOff && fourOff) {
+		pulse = duty[14];
+		enable = 1;
+		kers = 0;
+	}
+	OCR0A = pulse;
+	OCR1A = pulse;
+	OCR2A = pulse;
+}
+
+int main(void) {
+
+	//debug
+	DDRB |= (1 << PB4);
+	//speedometer
+	DDRB |= (1 << PB6);
+
+	DDRB |= (1 << PB1);	//SD PWM outs
+	DDRD |= (1 << PD6);
+	DDRB |= (1 << PB3);
+
+	DDRD |= (1 << PD1);	//IN step outs
+	DDRD |= (1 << PD2);
+	DDRD |= (1 << PD3);
+
+	DDRC &= ~(1 << PC1);	//hall ins
+	DDRC &= ~(1 << PC2);
+	DDRC &= ~(1 << PC3);
+
+	//PORTC |= (1 << PC1);	//pull-up
+	//PORTC |= (1 << PC2);
+	//PORTC |= (1 << PC3);
+
+	DDRB &= ~(1 << PB7);	//bus ins
+	DDRD &= ~(1 << PD5);
+	DDRD &= ~(1 << PD7);
+	DDRB &= ~(1 << PB0);
+
+	PORTB |= (1 << PB7);	//pull-up
+	PORTD |= (1 << PD5);
+	PORTD |= (1 << PD7);
+	PORTB |= (1 << PB0);
+
+	DDRC |= (1 << PC5);
+
+	//ADC define
+	ADMUX |= (1 << REFS0);
+	//prescaler
+	//ADCSRA |= (1 << ADPS1);//125kHz
+	//ADCSRA |= (1 << ADPS2);
+
+	ADCSRA |= (1 << ADPS0);		//1MHz
+	ADCSRA |= (1 << ADPS1);
+
+	ADCSRA |= (1 << ADEN); //ON
+
+	//PWM define
+	//timer 0
+	OCR0A = 0;
+	TCCR0A &= ~(1 << COM0A1);	//pwm off
+
+	TCCR0A |= (1 << WGM01);	//mode
+	TCCR0A |= (1 << WGM00);
+
+	TCCR0B |= (1 << CS00);	//prescaler
+	TCCR0B |= (1 << CS01);
+
+	//timer1
+	OCR1A = 0; //pwm out LOW
+	TCCR1A &= ~(1 << COM1A1); //pwm off
+
+	TCCR1A |= (1 << WGM10);	//mode
+	TCCR1B |= (1 << WGM12);
+
+	TCCR1B |= (1 << CS10); //prescaler-freq= clock/prescaler/256
+	TCCR1B |= (1 << CS11);
+
+	//timer2
+	OCR2A = 0;
+	TCCR2A &= ~(1 << COM2A1); //pwm off
+
+	TCCR2A |= (1 << WGM20); //mode
+	TCCR2A |= (1 << WGM21);
+
+	TCCR2B |= (1 << CS22);
+	TCCR2B &= ~(1 << CS21);
+
+	while (1) {
+		measurement();
+		power();
+
+		if ((sensor1 == 1) && (sensor2 == 0) && (sensor3 == 1)) {
+			PORTB |= (1 << PB6); ///speedo pulse
+			PORTB |= (1 << PB4);
+		} else {
+			PORTB &= ~(1 << PB6);
+			PORTB &= ~(1 << PB4);
+		}
+
+		if (enable == 1 && kers == 0) {
+
+			/*TCCR0B |= (1 << CS00);	//prescalers to 400Hz
+			 TCCR0B |= (1 << CS01);
+			 TCCR1B |= (1 << CS10);
+			 TCCR1B |= (1 << CS11);
+			 TCCR2B &= ~(1 << CS20);
+			 TCCR2B |= (1 << CS22);
+			 TCCR2B &= ~(1 << CS21);*/
+
+			TCCR0B |= (1 << CS00);	//prescalers to 30Khz
+			TCCR0B &= ~(1 << CS01);
+			TCCR1B |= (1 << CS10);
+			TCCR1B &= ~(1 << CS11);
+			TCCR2B |= (1 << CS20);
+			TCCR2B &= ~(1 << CS22);
+			TCCR2B &= ~(1 << CS21);
+
+			if ((sensor1 == 1) && (sensor2 == 0) && (sensor3 == 1)) {
+				rotation(1);
+				phase = 1;
+			}
+
+			if ((sensor1 == 1) && (sensor2 == 0) && (sensor3 == 0)) {
+				rotation(2);
+				phase = 2;
+			}
+
+			if ((sensor1 == 1) && (sensor2 == 1) && (sensor3 == 0)) {
+				rotation(3);
+				phase = 3;
+			}
+
+			if ((sensor1 == 0) && (sensor2 == 1) && (sensor3 == 0)) {
+				rotation(4);
+				phase = 4;
+			}
+
+			if ((sensor1 == 0) && (sensor2 == 1) && (sensor3 == 1)) {
+				rotation(5);
+				phase = 5;
+			}
+
+			if ((sensor1 == 0) && (sensor2 == 0) && (sensor3 == 1)) {
+				rotation(0);
+				phase = 0;
+			}
+
+			if (((sensor1 == 1) && (sensor2 == 1) && (sensor3 == 1))
+					|| ((sensor1 == 0) && (sensor2 == 0) && (sensor3 == 0))) {
+				ALL_OFF
+				_delay_ms(1000);
+			}
+		} else if (enable == 0 && kers == 1) {
+
+			TCCR0B &= ~(1 << CS00);	//prescalers to 4kHz
+			TCCR0B |= (1 << CS01);
+			TCCR1B &= ~(1 << CS10);
+			TCCR1B |= (1 << CS11);
+			TCCR2B &= ~(1 << CS20);
+			TCCR2B &= ~(1 << CS22);
+			TCCR2B |= (1 << CS21);
+
+			OCR1A = kersPower;
+			TCCR1A |= (1 << COM1A1);
+			PORTD &= ~(1 << PD1);
+
+			OCR0A = kersPower;
+			TCCR0A |= (1 << COM0A1);
+			PORTD &= ~(1 << PD2);
+
+			OCR2A = kersPower;
+			TCCR2A |= (1 << COM2A1);
+			PORTD &= ~(1 << PD3);
+
+		} else {
+			ALL_OFF
+		}
+
+	}
+}
---
+++ driver/main.c
@@ -0,0 +1,936 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 lut 2018
+ *      Author: Bartosz Pracz
+ *     	blink-1 driver
+
+ *      v0.2 - working, first test
+ *      v0.3- beta amperometer
+ *      v0.4- custom characters, notify
+ *      v0.5- kers support
+ *     	v0.6- better security
+ *     	v0.7- throttle steps as array
+ *     	v0.8- upgrade to 1.5kW :)
+ *     	v0.9-fixed amperometer
+ *     	v1.1- more amp measurement options
+ *     	v1.2.1- fixed for ACS712 with current divider, display refresh, faster speedo
+ *     	v1.3 - 2kW :)
+ *     	v1.4 - fixed overdraining battery, faster screen refresh
+ *     	v1.5 - safeLoop notify, faster speedo, Overload notify
+ *     	Milestone 1- All working?
+ *     	v2.0 - added menu
+ *     	v2.0.1 - better overload secure
+ *     	v2.1 - speed limit, optimalization.
+ *     	v2.2 -speed limit fix
+ *     	v2.3 - speed limit fix x2?
+ *     	2.4 speedo limit again, better amp count.
+ *     	2.5 delta upgrade
+ *     	2.6 temp sensor
+ *
+ */
+
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <util/delay.h>
+#include "hd44780.h"
+#include <avr/interrupt.h>
+
+EEMEM unsigned int s; // speedo
+EEMEM unsigned int a; // amperage
+EEMEM unsigned int v; // voltage
+EEMEM unsigned int l; // speed limit
+EEMEM unsigned int m; // mode
+
+#define one PORTD |= (1<<PD0)
+#define two PORTD |= (1<<PD1)
+#define three PORTD |= (1<<PD2)
+#define four PORTB |= (1<<PB6)
+
+#define oneOff PORTD &= ~(1<<PD0)
+#define twoOff PORTD &= ~(1<<PD1)
+#define threeOff PORTD &= ~(1<<PD2)
+#define fourOff PORTB &= ~(1<<PB6)
+
+//variables
+//main
+volatile int throttle = 0;
+volatile int duty = 0;
+volatile int throStep[] = { 210, 250, 300, 350, 400, 450, 500, 550, 600, 650,
+		675, 690, 720, 750 };
+int maxDuty = 15;
+
+//modes
+int mode;
+int kers = 0;
+int ecoLimit = 9;
+int smartLimit = 12;
+
+//menu
+int menu = 0;
+int option = 0;
+int speedoCal;
+int ampCal;
+int voltCal;
+int speedLimitCal;
+float minVoltageDef = 39;
+int maxAmperageDef = 40;
+int speedoDef = 15;
+int speedLimitDef = 99;
+int modeDef = 3;
+
+//voltages
+volatile float voltage;
+volatile float difference;
+int minVoltage;
+
+//amperages
+volatile int amperage = 0;
+int maxAmperage;
+
+//temp
+int temp;
+int maxTemp = 60;
+
+//lcd
+int modeDisplay = 0;
+int voltageDisplay = 11;
+int throttleDisplay = 67;
+int kersDisplay = 65;
+int ampDisplay = 6;
+int speedDisplay = 70;
+int notifyDisplay = 76;
+char buffer[10];
+int counter = 1;
+
+//speedometer
+volatile int count = 0;
+volatile int speed;
+int speedo;
+int speedLimit;
+int speedWait;
+
+int itoa();
+int sprintf(char *str, const char *format, ...);
+
+void tempCheck(void) {
+	ADMUX |= (1 << MUX2);
+	ADMUX &= ~(1 << MUX1);		//ADC5(temp)
+	ADMUX |= (1 << MUX0);
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	temp = ((ADC * 5) / 1024) * 100;
+}
+
+void batteryCheck(void) {
+	ADMUX &= ~(1 << MUX2);
+	ADMUX |= (1 << MUX1);		//ADC3(battery)
+	ADMUX |= (1 << MUX0);
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	voltage = ADC / 14.4;
+}
+
+void throttleCheck() {
+	ADMUX &= ~(1 << MUX2);
+	ADMUX |= (1 << MUX1);		//ADC2(throttle)
+	ADMUX &= ~(1 << MUX0);
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	throttle = ADC;
+}
+
+void ampCheck() {
+	ADMUX &= ~(1 << MUX2);
+	ADMUX &= ~(1 << MUX1);		//ADC0(amp) 205/V, 925/4,5V
+	ADMUX &= ~(1 << MUX0);
+	ADCSRA |= (1 << ADSC); //start
+	while (ADCSRA & (1 << ADSC))
+		;
+	//amperage = ADC / 7 - 73; //for 2xACS712-30A
+	//amperage = ADC /14 - 36; //for ACS712-30A
+	//amperage = ADC / 20 - 25; //for ACS712-20A
+	//amperage =( ADC /14 - 36) * 5; //for ACS712-30A with divider
+	amperage = ADC / 4 - 127; //for ACS712-20A with divider
+}
+
+void lcdSplash() {
+	lcd_init();
+	lcd_clrscr();
+	lcd_puts("Bart's");
+	lcd_goto(64);
+	lcd_puts("blink-1");
+	_delay_ms(1000);
+}
+
+void lcdRef() {
+	lcd_init();
+
+	//right
+	lcd_command(_BV(LCD_CGRAM) + 0 * 8);
+	lcd_putc(0b00000);
+	lcd_putc(0b10000);
+	lcd_putc(0b11100);
+	lcd_putc(0b11111);
+	lcd_putc(0b11100);
+	lcd_putc(0b10000);
+	lcd_putc(0b00000);
+	lcd_putc(0b00000);
+	lcd_goto(0);
+
+	//left
+	lcd_command(_BV(LCD_CGRAM) + 1 * 8);
+	lcd_putc(0b00000);
+	lcd_putc(0b00001);
+	lcd_putc(0b00111);
+	lcd_putc(0b11111);
+	lcd_putc(0b00111);
+	lcd_putc(0b00001);
+	lcd_putc(0b00000);
+	lcd_putc(0b00000);
+	lcd_goto(0);
+
+	//sep
+	lcd_command(_BV(LCD_CGRAM) + 2 * 8);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_goto(0);
+
+	//weak
+	lcd_command(_BV(LCD_CGRAM) + 3 * 8);
+	lcd_putc(0b01110);
+	lcd_putc(0b11011);
+	lcd_putc(0b10001);
+	lcd_putc(0b10001);
+	lcd_putc(0b10001);
+	lcd_putc(0b11111);
+	lcd_putc(0b11111);
+	lcd_putc(0b00000);
+	lcd_goto(0);
+
+	//temp
+	lcd_command(_BV(LCD_CGRAM) + 4 * 8);
+	lcd_putc(0b00100);
+	lcd_putc(0b01100);
+	lcd_putc(0b00100);
+	lcd_putc(0b01100);
+	lcd_putc(0b00100);
+	lcd_putc(0b01110);
+	lcd_putc(0b01110);
+	lcd_putc(0b00000);
+	lcd_goto(0);
+
+	//over
+	lcd_command(_BV(LCD_CGRAM) + 5 * 8);
+	lcd_putc(0b00100);
+	lcd_putc(0b00100);
+	lcd_putc(0b00100);
+	lcd_putc(0b00100);
+	lcd_putc(0b10101);
+	lcd_putc(0b01110);
+	lcd_putc(0b00100);
+	lcd_putc(0b00000);
+	lcd_goto(0);
+
+	lcd_clrscr();
+	lcd_goto(66);
+	lcd_putc(2);
+	lcd_goto(68);
+	lcd_puts("D");
+}
+
+void power(int power) {
+
+	if (power == 0) {
+		one;
+		two;
+		three;
+		four;
+	}
+	if (power == 1) {
+		one;
+		two;
+		three;
+		fourOff;
+	}
+	if (power == 2) {
+		one;
+		two;
+		threeOff;
+		four;
+	}
+	if (power == 3) {
+		one;
+		two;
+		threeOff;
+		fourOff;
+	}
+	if (power == 4) {
+		one;
+		twoOff;
+		three;
+		four;
+	}
+	if (power == 5) {
+		one;
+		twoOff;
+		three;
+		fourOff;
+	}
+	if (power == 6) {
+		one;
+		twoOff;
+		threeOff;
+		four;
+	}
+	if (power == 7) {
+		one;
+		twoOff;
+		threeOff;
+		fourOff;
+	}
+	if (power == 8) {
+		oneOff;
+		two;
+		three;
+		four;
+	}
+	if (power == 9) {
+		oneOff;
+		two;
+		three;
+		fourOff;
+	}
+	if (power == 10) {
+		oneOff;
+		two;
+		threeOff;
+		four;
+	}
+	if (power == 11) {
+		oneOff;
+		two;
+		threeOff;
+		fourOff;
+	}
+	if (power == 12) {
+		oneOff;
+		twoOff;
+		three;
+		four;
+	}
+	if (power == 13) {
+		oneOff;
+		twoOff;
+		three;
+		fourOff;
+	}
+	if (power == 14) {
+		oneOff;
+		twoOff;
+		threeOff;
+		four;
+	}
+	if (power == 15) {
+		oneOff;
+		twoOff;
+		threeOff;
+		fourOff;
+	}
+}
+
+void dNotify() {
+
+	if ((counter % 500) == 0) {
+		lcd_goto(notifyDisplay);
+		lcd_puts("    ");
+	}
+
+	if (voltage < minVoltage + 2) {
+		lcd_goto(notifyDisplay);
+		lcd_putc(3);
+	}
+
+	if (amperage > maxAmperage) {
+		lcd_goto(notifyDisplay + 1);
+		lcd_putc(5);
+	}
+	if (temp > maxTemp) {
+		lcd_goto(notifyDisplay + 2);
+		lcd_putc(4);
+	}
+
+}
+
+void dAmpers() {
+	//display amperage
+	if (amperage >= 10) {
+		itoa(amperage, buffer, 10);
+		lcd_goto(ampDisplay);
+		lcd_puts("     ");
+		lcd_goto(ampDisplay);
+		lcd_puts(buffer);
+		lcd_puts("A");
+	}
+
+	if (amperage < 10 && amperage >= 0) {
+		itoa(amperage, buffer, 10);
+		lcd_goto(ampDisplay);
+		lcd_puts("     ");
+		lcd_goto(ampDisplay);
+		lcd_puts(buffer);
+		lcd_puts("A");
+	}
+	if (amperage < 0 && amperage > -10) {
+		itoa(amperage, buffer, 10);
+		lcd_goto(ampDisplay);
+		lcd_puts("     ");
+		lcd_goto(ampDisplay);
+		lcd_puts(buffer);
+		lcd_puts("A");
+	}
+	if (amperage <= -10) {
+		itoa(amperage, buffer, 10);
+		lcd_goto(ampDisplay);
+		lcd_puts("     ");
+		lcd_goto(ampDisplay);
+		lcd_puts(buffer);
+		lcd_puts("A");
+	}
+}
+
+void dVolts() {
+	//display voltage
+	if (voltage >= 10) {
+		lcd_goto(voltageDisplay);
+		sprintf(buffer, "%2.1fV", voltage);
+		lcd_puts(buffer);
+		lcd_puts("   ");
+	}
+
+	if (voltage < 10) {
+		lcd_goto(voltageDisplay);
+		lcd_puts(" ");
+		sprintf(buffer, "%1.1fV", voltage);
+		lcd_puts(buffer);
+		lcd_puts("   ");
+	}
+}
+
+void dThrottle() {
+	//display throttle
+
+	lcd_goto(throttleDisplay);
+	if (throttle < throStep[0])
+		lcd_puts(" ");
+	if (throttle >= throStep[0])
+		lcd_putc(0);
+}
+
+void dKers() {
+
+	lcd_goto(kersDisplay);
+	if (amperage < 0) {
+		lcd_putc(1);
+	} else {
+		lcd_puts(" ");
+	}
+}
+
+void dSpeed() {
+	itoa(speed, buffer, 10);
+	lcd_goto(speedDisplay);
+	;
+	lcd_puts(buffer);
+	if (speed < 10)
+		lcd_puts("kmh ");
+	if (speed >= 10)
+		lcd_puts("kmh");
+}
+
+void modes() {
+//mode select
+	if (!(PINB & (1 << PB4))) {
+
+		_delay_ms(800);
+		throttleCheck();
+
+		if (throttle <= throStep[0]) {
+
+			if (!(PINB & (1 << PB4))) {
+				if (kers == 1) {
+					kers = 0;
+					lcd_goto(64);
+					lcd_puts(" ");
+					_delay_ms(800);
+				} else if (kers == 0) {
+					kers = 1;
+					lcd_goto(64);
+					lcd_puts("K");
+					_delay_ms(800);
+				}
+			} else {
+
+				if (mode <= 2) {
+					mode++;
+					_delay_ms(800);
+				} else {
+					mode = 1;
+					_delay_ms(800);
+				}
+				eeprom_write_byte(&m, mode);
+				_delay_ms(20);
+			}
+
+		}
+
+		else {
+			menu = 1;
+			lcd_clrscr();
+			lcd_goto(0);
+			lcd_puts("blink-1. menu:");
+			_delay_ms(800);
+
+			while (menu == 1) {
+
+				power(1);
+				throttleCheck();
+				if (throttle < throStep[1]) {
+					lcd_goto(64);
+					lcd_puts("speedo calibrate");
+					option = 1;
+				}
+				if (throttle >= throStep[1] && throttle < throStep[3]) {
+					lcd_goto(64);
+					lcd_puts("max amperage    ");
+					option = 2;
+				}
+
+				if (throttle >= throStep[3] && throttle < throStep[5]) {
+					lcd_goto(64);
+					lcd_puts("minimal voltage  ");
+					option = 3;
+				}
+				if (throttle >= throStep[5] && throttle < throStep[7]) {
+					lcd_goto(64);
+					lcd_puts("speed limit      ");
+					option = 4;
+
+				}
+				if (throttle >= throStep[7] && throttle < throStep[9]) {
+					lcd_goto(64);
+					lcd_puts("check values     ");
+					option = 5;
+				}
+				if (throttle >= throStep[9] && throttle < throStep[11]) {
+					lcd_goto(64);
+					lcd_puts("reset defaults   ");
+					option = 6;
+				}
+				if (throttle >= throStep[11]) {
+					lcd_goto(64);
+					lcd_puts("exit             ");
+					option = 7;
+				}
+				if (!(PINB & (1 << PB4))) {
+
+					_delay_ms(800);
+					if (option == 7) {
+						throttleCheck();
+						while (throttle > throStep[0]) {
+							lcd_goto(64);
+							lcd_puts("release throttle");
+							throttleCheck();
+						}
+						menu = 0;
+						lcdRef();
+						maxDuty = 15;
+					}
+					while (option == 1) {
+						lcd_goto(64);
+						lcd_puts("speedo Cal:   ");
+						lcd_goto(78);
+						itoa(speedoCal, buffer, 10);
+						lcd_puts(buffer);
+						throttleCheck();
+						speedoCal = throttle / 40;
+						if (speedoCal < 10)
+							speedoCal = 10;
+						if (!(PINB & (1 << PB4))) {
+							speedo = speedoCal;
+							lcd_goto(64);
+							lcd_puts("set: ");
+							itoa(speedo, buffer, 10);
+							lcd_puts(buffer);
+							lcd_puts("         ");
+							_delay_ms(800);
+							option = 0;
+							eeprom_write_byte(&s, speedo);
+							_delay_ms(20);
+
+						}
+					}
+					while (option == 2) {
+						lcd_goto(64);
+						lcd_puts("max Amp:      ");
+						lcd_goto(78);
+						itoa(ampCal, buffer, 10);
+						lcd_puts(buffer);
+						if (ampCal <= 9)
+							lcd_puts(" ");
+						throttleCheck();
+						ampCal = (throttle / 7) - 22;
+						if (ampCal > 99)
+							ampCal = 99;
+						if (!(PINB & (1 << PB4))) {
+							maxAmperage = ampCal;
+							lcd_goto(64);
+							lcd_puts("set: ");
+							itoa(maxAmperage, buffer, 10);
+							lcd_puts(buffer);
+							lcd_puts("         ");
+							_delay_ms(800);
+							option = 0;
+							eeprom_write_byte(&a, maxAmperage);
+							_delay_ms(20);
+
+						}
+					}
+
+					while (option == 3) {
+						lcd_goto(64);
+						lcd_puts("min Volt:     ");
+						lcd_goto(78);
+						itoa(voltCal, buffer, 10);
+						lcd_puts(buffer);
+						throttleCheck();
+						if (throttle < throStep[0])
+							voltCal = 30;
+						if (throttle >= throStep[1] && throttle < throStep[4])
+							voltCal = 39;
+						if (throttle >= throStep[4] && throttle < throStep[8])
+							voltCal = 60;
+						if (!(PINB & (1 << PB4))) {
+							minVoltage = voltCal;
+							lcd_goto(64);
+							lcd_puts("set: ");
+							itoa(minVoltage, buffer, 10);
+							lcd_puts(buffer);
+							lcd_puts("         ");
+							_delay_ms(800);
+							option = 0;
+							eeprom_write_byte(&v, minVoltage);
+							_delay_ms(20);
+
+						}
+					}
+
+					while (option == 5) {
+						lcd_goto(64);
+						lcd_puts("speedo:       ");
+						lcd_goto(78);
+						itoa(speedo, buffer, 10);
+						lcd_puts(buffer);
+						_delay_ms(800);
+
+						lcd_goto(64);
+						lcd_puts("Max amperage: ");
+						lcd_goto(78);
+						itoa(maxAmperage, buffer, 10);
+						lcd_puts(buffer);
+						lcd_puts("    ");
+						_delay_ms(800);
+
+						lcd_goto(64);
+						lcd_puts("min Voltage:  ");
+						lcd_goto(78);
+						itoa(minVoltage, buffer, 10);
+						lcd_puts(buffer);
+						_delay_ms(800);
+						option = 0;
+
+						lcd_goto(64);
+						lcd_puts("speed limit:  ");
+						lcd_goto(78);
+						itoa(speedLimit, buffer, 10);
+						lcd_puts(buffer);
+						_delay_ms(800);
+						option = 0;
+
+					}
+
+					while (option == 6) {
+						lcd_goto(64);
+						lcd_puts("restoring...    ");
+						speedo = speedoDef;
+						maxAmperage = maxAmperageDef;
+						minVoltage = minVoltageDef;
+						speedLimit = speedLimitDef;
+						eeprom_write_byte(&s, speedo);
+						_delay_ms(20);
+						eeprom_write_byte(&a, maxAmperage);
+						_delay_ms(20);
+						eeprom_write_byte(&v, minVoltage);
+						_delay_ms(20);
+						eeprom_write_byte(&l, speedLimit);
+						_delay_ms(20);
+						_delay_ms(800);
+						option = 0;
+
+					}
+
+					while (option == 4) {
+						lcd_goto(64);
+						lcd_puts("Speed Limit:  ");
+						lcd_goto(78);
+						itoa(speedLimitCal, buffer, 10);
+						lcd_puts(buffer);
+						if (speedLimitCal <= 9)
+							lcd_puts(" ");
+						throttleCheck();
+						speedLimitCal = (throttle / 7) - 22;
+						if (speedLimitCal > 99)
+							speedLimitCal = 99;
+						if (!(PINB & (1 << PB4))) {
+							speedLimit = speedLimitCal;
+							lcd_goto(64);
+							lcd_puts("set: ");
+							itoa(speedLimit, buffer, 10);
+							lcd_puts(buffer);
+							lcd_puts("         ");
+							_delay_ms(800);
+							option = 0;
+							eeprom_write_byte(&l, speedLimit);
+							_delay_ms(20);
+
+						}
+					}
+
+				}
+			}
+		}
+	}
+
+//mode
+	if (mode == 1) {
+		lcd_goto(modeDisplay);
+		lcd_puts("eco  ");
+	}
+
+	if (mode == 2) {
+		lcd_goto(modeDisplay);
+		lcd_puts("smart");
+	}
+
+	if (mode == 3) {
+		lcd_goto(modeDisplay);
+		lcd_puts("sport");
+	}
+	if (kers == 1) {
+		lcd_goto(64);
+		lcd_puts("K");
+	}
+	if (kers == 0) {
+		lcd_goto(64);
+		lcd_puts(" ");
+	}
+
+}
+
+int main(void) {
+
+//debug led(kers ON)
+//DDRC |= (1 << PC5);
+//DDRC |= (1 << PC4);
+
+//button
+	DDRB &= ~(1 << PB4);
+	PORTB |= (1 << PB4);
+
+//ADC define
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS1); //prescaler
+	ADCSRA |= (1 << ADPS2);
+	ADCSRA |= (1 << ADEN); //ON
+//ADMUX |= (1 << REFS0); //reference voltage
+//pin select
+	ADMUX |= (1 << MUX1);	//ADC3(battery)
+	ADMUX |= (1 << MUX0);
+
+	ADMUX |= (1 << MUX1);	//ADC2(throttle)
+	ADMUX &= ~(1 << MUX0);
+
+	ADMUX |= (1 << MUX0);	//ADC1(kers)
+	ADMUX &= ~(1 << MUX1);
+
+//data bus for BLDC driver
+	DDRD |= (1 << PD0);
+	DDRD |= (1 << PD1);
+	DDRD |= (1 << PD2);
+	DDRB |= (1 << PB6);
+
+	PORTD |= (1 << PD0);
+	PORTD |= (1 << PD1);
+	PORTD |= (1 << PD2);
+	PORTB |= (1 << PB6);
+
+	lcdSplash();
+	lcdRef();
+
+//speedometer
+
+	EICRA |= (1 << ISC11); // falling edge on INT1
+	EIMSK |= (1 << INT1); //enable interrupts on INT1
+	TCCR1B |= (1 << WGM12); //CTC, OCR1A
+	TCCR1B |= (1 << CS12); //set prescaler for 1024
+	TCCR1B |= (1 << CS10);
+
+	TIMSK1 |= (1 << OCIE1A);
+	sei();
+
+	//OCR1A = 7812;
+	OCR1A = 3906;
+	DDRD &= ~(1 << PD3);
+	PORTD |= (1 << PD3);
+
+	//read eeprom
+	minVoltage = eeprom_read_byte(&v);
+	if (minVoltage > 60 || minVoltage < 28)
+		minVoltage = minVoltageDef;
+	maxAmperage = eeprom_read_byte(&a);
+	if (maxAmperage > 99)
+		maxAmperage = maxAmperageDef;
+	speedo = eeprom_read_byte(&s);
+	if (speedo > 50)
+		speedo = speedoDef;
+	speedLimit = eeprom_read_byte(&l);
+	if (speedLimit > 99)
+		speedLimit = speedLimitDef;
+	mode = eeprom_read_byte(&m);
+	if (mode > 3)
+		mode = modeDef;
+
+	while (1) {
+
+		modes();
+
+		batteryCheck();
+		ampCheck();
+		throttleCheck();
+		tempCheck();
+
+		dThrottle();
+		dKers();
+		dNotify();
+
+		//Non Continous operations
+
+		counter++;
+		if (counter > 32000)
+			counter = 1;
+
+		if ((counter % 1000) == 0) {
+			lcdRef();
+		}
+
+		if ((counter % 50) == 0)
+			if (maxDuty < 15)
+				maxDuty++; //slow maxDuty reset
+
+		if ((counter % 100) == 0) {
+			dVolts();
+			dAmpers();
+		}
+
+		if ((counter % 50) == 0) {
+			dSpeed();
+		}
+
+		//speedLimit
+
+		if ((counter % 20) == 0) {
+			if (speed > speedLimit && duty > 1 && speedWait == 0) {
+				maxDuty = duty - 3;
+				speedWait = 1;
+			}
+		}
+
+		//overload secure, over discharge secure
+		if ((amperage > maxAmperage || voltage < minVoltage) && duty > 1) {
+			maxDuty = duty - 1;
+		}
+
+		//end of non continous operation
+
+		//send signal
+		if (throttle < throStep[0] && kers == 1) {
+			power(0); //when power zero, send 0 enable and 1 kers
+		} else if (throttle < throStep[0] && kers == 0) {
+			power(1); //wher power one, send 0 enable and 0 kers
+
+		} else {
+			if (throttle >= throStep[0])
+				duty = 2;
+			if (throttle >= throStep[1])
+				duty = 3;
+			if (throttle >= throStep[2])
+				duty = 4;
+			if (throttle >= throStep[3])
+				duty = 5;
+			if (throttle >= throStep[4])
+				duty = 6;
+			if (throttle >= throStep[5])
+				duty = 7;
+			if (throttle >= throStep[6])
+				duty = 8;
+			if (throttle >= throStep[7])
+				duty = 9;
+			if (throttle >= throStep[8])
+				duty = 10;
+			if (throttle >= throStep[9])
+				duty = 11;
+			if (throttle >= throStep[10])
+				duty = 12;
+			if (throttle >= throStep[11])
+				duty = 13;
+			if (throttle >= throStep[12])
+				duty = 14;
+			if (throttle >= throStep[13])
+				duty = 15;
+
+			if (mode == 1 && duty > 7) //eco mode
+				duty = 7;
+			if (mode == 2 && duty > 10) //smart mode
+				duty = 10;
+
+			if (duty > maxDuty)
+				duty = maxDuty;
+
+			power(duty); //send power pattern
+
+		}
+	}
+}
+
+ISR(INT1_vect) {
+//CPU Jumps here automatically when INT0 pin detect a falling edge
+	count++;
+}
+
+ISR(TIMER1_COMPA_vect) {
+//CPU Jumps here every 1 sec exactly!
+	speed = (count * 10) / speedo;
+	if (speed > 99)
+		speed = 99;
+	count = 0;
+	speedWait = 0;
+}
